6708db202ef634d107b2d0cdcac0c2bb337de67a
Daniel Osorio <danielosorio83@gmail.com>
2017-10-25 10:08:26 -0500

    Setting up the index

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="title">Goal Coach</div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDom.render(
+  <App />,
+  document.getElementById('root')
+);
